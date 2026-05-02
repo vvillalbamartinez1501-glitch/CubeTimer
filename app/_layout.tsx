9d90fa6e316b8d4f3f89a3ca2127346ec86dea95
V Villalba Martínez <vvillalbamartinez1501@gmail.com>
2026-05-02 02:46:22 +0200

    feat: add RootLayout with theme provider and i18n support, and update web output mode to single

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,10 +8,6 @@ import '../src/i18n';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
@@ -19,7 +15,6 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
